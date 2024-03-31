bda16e4331b440f9519f52e3105b31acd60e6604
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-03-31 15:56:45 -0300

    Criando Entity Post com user aninhado

---
+++ src/main/java/com/pavaneli/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.pavaneli.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pavaneli.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
