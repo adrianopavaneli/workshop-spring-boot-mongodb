12ff65e3c0f0725c0243b423096adaa03457aff9
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-03-31 17:15:45 -0300

    Consulta simples com Query Methods

--- src/main/java/com/pavaneli/workshopmongo/resoures/PostResources.java
+++ src/main/java/com/pavaneli/workshopmongo/resoures/PostResources.java
@@ -1,13 +1,18 @@
 package com.pavaneli.workshopmongo.resoures;
 
+import java.net.URLDecoder;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pavaneli.workshopmongo.domain.Post;
+import com.pavaneli.workshopmongo.resoures.util.URL;
 import com.pavaneli.workshopmongo.services.PostService;
 
 @RestController
@@ -23,6 +28,14 @@ public class PostResources {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(value="/titlesearch",method =RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 	
 
 }
